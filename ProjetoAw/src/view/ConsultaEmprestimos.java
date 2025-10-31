210a5f1cfea074db511c2bbd38ec16844ce899a3
cecilyagoulart <anacecilyag@gmail.com>
2025-10-31 08:55:28 -0300

    Change JFrame close operation to DISPOSE_ON_CLOSE
    alterei pra não fechar a janela principal quando fechar a de consultar emprestimo

--- ProjetoAw/src/view/ConsultaEmprestimos.java
+++ ProjetoAw/src/view/ConsultaEmprestimos.java
@@ -30,7 +30,7 @@ public class ConsultaEmprestimos extends JFrame {
 
 	
 	public ConsultaEmprestimos(List <ConsultaM> consulta) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -39,3 +39,4 @@ public class ConsultaEmprestimos extends JFrame {
 	}
 
 }
+
